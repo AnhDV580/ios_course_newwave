8e6f505009d441da99974d2a930930cc78c5763d
anhdv <anhdv@newwave.vn>
2021-10-02 14:16:18 +0700

    exercise 2.2 (done)

--- lesson_2/exercise_2_2/exercise_2_2/ViewController.swift
+++ lesson_2/exercise_2_2/exercise_2_2/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var buttonChangeImage: UIButton!
+    @IBOutlet weak var imageName: UILabel!
+    
+    var listImageName: [String] = []
+    var currentNumber: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initData()
+        displayImage()
     }
 
-
+    @IBAction func changeImage(_ sender: Any) {
+        currentNumber = getRandomNumber()
+        displayImage()
+    }
+    
+    func getRandomNumber() -> Int {
+        return Int.random(in: 0...(listImageName.count - 1), excluding: currentNumber)
+    }
+    
+    func initData() -> Void {
+        listImageName = ["Image 1", "Image 2", "Image 3", "Image 4", "Image 5", "Image 6", "Image 7"]
+    }
+    
+    func displayImage() -> Void {
+        let image = UIImage(named: listImageName[currentNumber])
+        imageName.text = listImageName[currentNumber]
+        imageView.image = image
+    }
 }
 
+extension Int {
+    static func random(in range: ClosedRange<Int>, excluding x: Int) -> Int {
+        if range.contains(x) {
+            let r = Int.random(in: Range(uncheckedBounds: (range.lowerBound, range.upperBound)))
+            return r == x ? range.upperBound : r
+        } else {
+            return Int.random(in: range)
+        }
+    }
+}
